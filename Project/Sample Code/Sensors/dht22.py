e79625d8738ba378ca902e2af59c370d0664475a
BuildTools <unconfigured@null.spigotmc.org>
2021-05-11 22:26:52 -0700

    Add Adafruit_Python_DHT sample code

--- Project/Sample Code/Sensors/dht22.py
+++ Project/Sample Code/Sensors/dht22.py
@@ -3,10 +3,10 @@
  
 import time
 import board
-import adafruit_dht
+import Adafruit_DHT
  
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D4)
+dhtDevice = Adafruit_DHT.DHT22(board.D4)
  
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
